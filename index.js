9268e8f0b81d78ce7ea8bf987edb224f70ad6ae4
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-11 21:48:40 +0600

    Index JS File Updated of Server Side

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,11 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-    res.send("SoloSphere Server Side is Running");
-})
+  res.send("SoloSphere Server Side is Running");
+});
 
 app.listen(port, () => {
-    console.log(`SoloSphere Server is Running on Port: ${port}`);
-})
+  console.log(`SoloSphere Server is Running on Port: ${port}`);
+});
